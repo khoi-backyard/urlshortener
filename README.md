79458af1355c72d0b68deb53ad49a106dc3e72be
khoi <khoiracle@gmail.com>
2019-04-22 15:16:12 +0700

    redirection handler

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ The list of redirection should be maintained in a command line tool, what can:
 - [ ] Implement append to the list: urlshorten configure -a dogs -u www.dogs.com
 - [ ] Implement remove from the list: urlshorten -d dogs
 - [x] List redirections: urlshorten -l
-- [ ] Run HTTP server on a given port: urlshorten run -p 8080
+- [x] Run HTTP server on a given port: urlshorten run -p 8080
 - [ ] Prints usage info: urlshorten -h
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 
 	yaml "gopkg.in/yaml.v2"
@@ -12,18 +14,11 @@ import (
 const configFile = "./config.yml"
 
 func main() {
-	listFlag := flag.Bool("l", false, "list the url mapping")
-
-	flag.Parse()
-
-	if *listFlag {
-		listURLMapping()
+	if len(os.Args) == 1 {
+		fmt.Println("Invalid syntax. Check manual")
 		return
 	}
 
-}
-
-func listURLMapping() {
 	bytes, err := ioutil.ReadFile(configFile)
 
 	if err != nil {
@@ -36,6 +31,48 @@ func listURLMapping() {
 		exitWithError(err)
 	}
 
+	runFlagSet := flag.NewFlagSet("run", flag.ExitOnError)
+	runPort := runFlagSet.Int("p", 8080, "port of the server")
+
+	listFlag := flag.Bool("l", false, "list the url mapping")
+
+	switch os.Args[1] {
+	case "run":
+		runFlagSet.Parse(os.Args[2:])
+	default:
+		flag.Parse()
+	}
+
+	// Run subcommand
+	if runFlagSet.Parsed() {
+		http.HandleFunc("/", redirectionHandler(urlMap))
+		port := fmt.Sprintf(":%d", *runPort)
+		fmt.Printf("Starting server on port %v\n", port)
+		log.Fatal(http.ListenAndServe(port, nil))
+		return
+	}
+
+	if flag.Parsed() {
+		if *listFlag {
+			printURLMapping(urlMap)
+			return
+		}
+	}
+
+}
+
+func redirectionHandler(urlMap map[string]string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path[1:]
+		if url, ok := urlMap[path]; ok {
+			http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+			return
+		}
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	}
+}
+
+func printURLMapping(urlMap map[string]string) {
 	for k, v := range urlMap {
 		fmt.Printf("%v - %v\n", k, v)
 	}
